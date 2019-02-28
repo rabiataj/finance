578aec36315242ae8c6300bcf5161aac869f8e79
rabiataj <rabiataj98@gmail.com>
2019-02-28 22:23:23 +0500

    transaction

--- app.js
+++ app.js
@@ -3,16 +3,36 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     functions = require('./utils/functions'),
     mongoose = require("mongoose"),
+    User      = require("./models/user"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
-    request   = require("request");
+    request   = require("request"),
+    Transaction = require("./models/transaction"),
+    Buy         = require("./models/buy"),
+    Sell        = require("./models/sell");
     //User          = require("./")
     app.set("view engine", "ejs");
 
+   //PASSPORT CONFIGRATION
+   app.use(require("express-session")({
+    secret : "once again",
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
+
+
+app.get("/", function(req, res) {
+   res.send("profile"); 
+});
 app.get("/Quote", function(req, res){
-    request("http://dev.markitondemand.com/Api/v2/Quote",)
+    request("http://dev.markitondemand.com/Api/v2/Quote", )
    res.render("Quote.ejs");
    //  res.send("landing page");
 });
@@ -35,13 +55,44 @@ app.get("/Quote", function(req, res){
 //     })
 // });
 
+//=============//
+//AUTH ROUTE
+//============//
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({ username:req.body.username});
+    User.register(newUser, req.body.password, function( err, user){
+        if (err){
+            console.log(err);
+            return res.render("register");}
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/Quote");
+        });
+
+    });
+});
 
 
 
+//HISTORY ROUTE//
+app.get("/transaction", function(req, res){
+    res.render("History.ejs");
+});
 
 
+//BUY ROUTE//
 
+app.get("/buy",  function(req, res){
+   res.render("Buy.ejs");
+});
 
+//SELL ROUTE//
+app.get("/sell",function(req, res) {
+    res.render("sell.ejs")
+})
 
 
 const port = process.env.PORT || 9000;
---
+++ models/transaction.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+// // SCHEMA SETUP
+// var transactionSchema = new mongoose.Schema({
+//     symbol: String,
+//     balance: 10000,
+//     date: String,
+//     author: {
+//         id : {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User"
+//         },
+//         username: String
+//     },
+//
+// });
+//
+// //MODEL
+// module.exports = mongoose.model("Campground", campgroundSchema);
